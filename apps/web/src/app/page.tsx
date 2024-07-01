2955b3260c36d1464f0d74b00bbd7247f5504278
henri <henri.me@skiff.com>
2024-07-01 15:48:16 +0200

    fixed the problem with tailwind-merge, idk how

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,9 @@ import {Button} from "ui"
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <Button />
+      <Button>
+        hey
+      </Button>
     </main>
   );
 }
