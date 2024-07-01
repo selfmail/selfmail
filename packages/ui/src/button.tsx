c0ef14dfc21fee0711f918eda84c7cc96929cbfc
henri <henri.me@skiff.com>
2024-07-01 13:42:29 +0200

    added the default libs for creating a component library

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 /**
- * The button.
- * @returns 
+ * The default button, configurable.
+ * @returns {React.JSX.Element}
  */
-export function Button() {
+export function Button(): React.JSX.Element {
     return (
         <button className="bg-blue-500">
             hello
