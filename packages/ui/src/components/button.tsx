12613003ebecd8d353ceeb26e9f5bfb2a9985cfa
henri <henri.me@skiff.com>
2024-07-01 10:56:45 +0200

    added ui lib

---
+++ packages/ui/src/components/button.tsx
@@ -0,0 +1,9 @@
+"use client"
+import "../styles/global.css"
+export default function Button() {
+    return (
+        <button className="bg-blue-500">
+            hello
+        </button>
+    )
+}
