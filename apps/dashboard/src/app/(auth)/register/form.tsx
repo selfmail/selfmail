084e9aadc242b491ad1c48f7c30a18a0406fc884
henri <henri.me@skiff.com>
2024-07-13 09:03:25 +0200

    added a register form

---
+++ apps/dashboard/src/app/(auth)/register/form.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+/**
+ * This is the form for the register page.
+ * This form is a client component, because
+ * we need the return value of the server
+ * action.
+ */
+export default function RegisterForm() {
+    return (
+        <form className="lg:w-[800px]">
+            
+        </form>
+    )
+}
