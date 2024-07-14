1c9d53008e9a66becb8074e8b4333d3c9f1ccfa2
henri <henri.me@skiff.com>
2024-07-14 09:34:18 +0200

    added the dialog and fixed some bugs

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,7 +1,16 @@
+import { Dialog, DialogContent, DialogTrigger } from "ui";
+
 export default function HomePage() {
   return (
     <main>
-      
+      <Dialog>
+        <DialogTrigger>
+          hey, das ist der Dialog
+        </DialogTrigger>
+        <DialogContent>
+          this is the content
+        </DialogContent>
+      </Dialog>
     </main>
   );
 }
---
+++ packages/ui/src/components/dialog.tsx
@@ -0,0 +1,157 @@
+"use client"
+
+import { cn } from "../cn"
+import { createContext, useContext, useEffect } from "react"
+import { create } from "zustand"
+
+
+const IdContext = createContext<number | undefined>(undefined)
+
+/*
+ * The dialog.
+ * The dialog is a dialog, where you can open more dialogs inside. This means, the
+ * old dialog gets blurred, and a new dialog is in the focus of the user.
+ * 
+ * The dialog uses it's inputs like the content, will show nothing, and puts the content
+ * into the root of the tree. This means, the dialog will be visible.
+ * 
+ * The state will be tracked with a zustand store.
+ */
+
+// The zustand store
+type state = {
+    open: boolean,
+    openId: number | undefined,
+    content: React.ReactNode | undefined,
+    classNames: string | undefined
+}
+type action = {
+    setOpen: (state: boolean) => void,
+    setOpenId: (state: number | undefined) => void,
+    setContent: (state: React.ReactNode | undefined) => void,
+    setClassNames: (state: string | undefined) => void
+}
+/**
+ * A zustand store for tracking the state of the dialog.
+ */
+export const useDialogStore = create<state & action>((set) => ({
+    open: false,
+    openId: undefined,
+    content: undefined,
+    setOpen: (state) => set({ open: state }),
+    setOpenId: (state: number | undefined) => set({ openId: state }),
+    setContent: (state) => set({ content: state }),
+    classNames: undefined,
+    setClassNames: (state) => set({classNames: state})
+}))
+
+
+// The components
+
+/**
+ * This is the provider for the dialog.
+ * Please use this provider inside of your body. It will render the children, 
+ * the dialog and the background for the dialog.
+ */
+function DialogProvider({ children }: { children: React.ReactNode }) {
+    const { open, content, setContent, setOpenId, setOpen, classNames } = useDialogStore()
+    return (
+        <>
+            {children}
+            {/* The actuall dialog */}
+            {open && content && (
+                <>
+                    <div className="w-full h-screen flex items-center justify-center fixed z-40 top-0">
+                        <div className="w-full h-screen absolute bg-[#00000070] backdrop-brightness-75 " onClick={() => {
+                            setContent(undefined)
+                            setOpenId(undefined)
+                            setOpen(false)
+                        }} onKeyDown={() => {
+                            setContent(undefined)
+                            setOpenId(undefined)
+                            setOpen(false)
+                        }} />
+                        {/*FIXME: This is your modal. You can style it now. */}
+                        <div className={cn("absolute z-50 text-black bg-white lg:min-w-[500px] rounded-lg p-2", classNames)}>
+                            {content}
+                        </div>
+                    </div>
+                </>
+            )}
+        </>
+    )
+}
+
+
+/**
+ *   This component is only for the look in your codebase, this means, all is nice together.
+ */
+function Dialog({ children }: { children: React.ReactNode }) {
+    return (
+        <IdContext.Provider value={Math.random()}>
+            {children}
+        </IdContext.Provider>
+    )
+}
+
+/**
+ * The dialog trigger, this is a button which will automatic show the dialog.
+ */
+interface TriggerProps extends React.HTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode
+}
+function DialogTrigger({ children, ...props }: TriggerProps) {
+    const { open, setOpen, setOpenId } = useDialogStore()
+    const id = useContext(IdContext)
+    return (
+        <button type="button" {...props} onClick={(e) => {
+            setOpen(!open)
+            setOpenId(id)
+
+            // running the onClick prop
+            if (props.onClick) {
+                props.onClick(e)
+            }
+        }}>
+            {children}
+        </button>
+    )
+}
+
+
+/**
+ * The dialog content. This component will not render. The content
+ * will be used inside an other element, which will be rendered on the root
+ * of the react tree.
+ */
+function DialogContent({ children, className }: { children: React.ReactNode, className: string }) {
+    const { open, openId, setContent, setClassNames } = useDialogStore()
+    const id = useContext(IdContext)
+
+    useEffect(() => {
+        if (open && openId === id) {
+            setContent(children)
+            setClassNames(className)
+        }
+    }, [children, open, setContent, openId, id, setClassNames, className])
+
+    return <>
+    </>
+}
+
+// components for the look
+interface DialogHeaderProps extends React.HTMLAttributes<HTMLHeadingElement> {
+    children: React.ReactNode
+}
+function DialogHeader({children, ...props}: DialogHeaderProps) {
+    return (
+        <div className="flex">
+            <h2 className={cn("", props.className)} {...props}>
+                {children}
+            </h2>
+        </div>
+    )
+}
+
+// export all of the components
+export { Dialog, DialogProvider, DialogTrigger, DialogContent, DialogHeader }
