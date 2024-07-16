e82a257e0a1a607c40f1280f4b4e03e7f4de0a0a
henri <henri.me@skiff.com>
2024-07-16 10:52:34 +0200

    added a first version of the header

--- apps/dashboard/src/app/(dashboard)/email/[email]/page.tsx
+++ apps/dashboard/src/app/(dashboard)/email/[email]/page.tsx
@@ -1,4 +1,6 @@
 import { db } from "database";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default async function Email({
@@ -17,8 +19,13 @@ export default async function Email({
     console.log(email)
     return (
         <div>
-            <header>
-                <h1 className="text-2xl">{email.sender}</h1>
+            <header className="flex items-center justify-between">
+                <div className="flex items-cener space-x-2">
+                    <div className="flex items-center">
+                        <Link href={"/"}><ChevronLeft className="w-4 h-4" /></Link>
+                    </div>
+                    <Link href={`/contacts/${email.sender}`}>{email.sender}</Link>
+                </div>
                 <p className="text-sm">{email.createdAt.toLocaleDateString()}</p>
             </header>
             <h1 className="text-2xl">{email.subject}</h1>
