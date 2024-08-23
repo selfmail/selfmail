13f633c27e138a6554f08edefae4f523fdbbdf76
henri <henri.me@skiff.com>
2024-08-23 10:40:22 +0200

    added a function to get the content from the object

--- packages/cli/commands/init.ts
+++ packages/cli/commands/init.ts
@@ -20,6 +20,8 @@ export const init = async () => {
 
     // check if the required packages are installed
 
+    consola.info("Checking if the required packages are installed... (git, nodejs, npm, pnpm)")
+
     // git
     await asyncExec('git --version').catch((err) => { if (err) handleError("Git is not installed or throw an error.") });
 
@@ -31,40 +33,70 @@ export const init = async () => {
 
     // pnpm
     await asyncExec('pnpm --version').catch((err) => { if (err) handleError("Pnpm is not installed or throw an error.") });
+    // success message
     consola.success("All required packages are installed.")
     space()
 
+    // loading the packages
     const spinner = ora("Cloning the repository...").start()
 
     // clone the repository
-    // await asyncExec(`git clone https://github.com/selfmail/selfmail.git ${new String(dir)}`).catch((err) => { if (err) handleError("We were unable to clone the repository.") });
+    await asyncExec(`git clone https://github.com/selfmail/selfmail.git ${new String(dir)}`).catch((err) => { if (err) handleError("We were unable to clone the repository.") });
     space()
-
 
     // installing the packages
     spinner.text = "Installing the packages..."
-    // await asyncExec(`cd ${new String(dir)} && pnpm install`).catch((err) => { if (err) handleError("We were unable to install the packages with pnpm.") });
+    await asyncExec(`cd ${new String(dir)} && pnpm install`).catch((err) => { if (err) handleError("We were unable to install the packages with pnpm.") });
 
     spinner.text = "Setting up the configuration file..."
-
+    await write(fileContent.config.name, await getContent(fileContent.config.content, {}))
 
     spinner.text = "Creating the .env file..."
-    await write(".env", `
-# The name of your application
-APP_NAME=selfmail
-# The domain of your application
-APP_DOMAIN=localhost:3000
-# The port of your application
-APP_PORT=3000
-# The email address of your application
-APP_EMAIL=selfmail@localhost
-# The password of your application
-APP_PASSWORD=selfmail
-`)
+
+    //TODO: create the .env file
+
+
+    consola.info("Please fill the .env file with the required information.")
 
     spinner.stop()
+}
+
+type Content = string | ((config?: { [key: string]: string }) => string | Promise<string>);
 
 
+// object to store the content for the files which are going to be created
+const fileContent: {
+    [key: string]: {
+        name: string,
+        content: Content
+    }
+} = {
+    config: {
+        name: "selfmail.config.ts",
+        content: `
+import type { Config } from "@selfmail/config"
 
+export const config: Config = {
+    dir: "",
+    name: "",
+    domain: "localhost:3000",
+    port: "3000",
+    email: "selfmail@localhost",
+    password: "selfmail",
+}
+        `,
+    }
+}
 
+async function getContent(path: Content, config?: { [key: string]: string }): Promise<string> {
+    if (typeof path === 'string') {
+        return path;
+    } else if (typeof path === "function") {
+        const result = path(config);
+        if (result instanceof Promise) {
+            return await result;
+        }
+        return result;
+    }
+    return handleError("Invalid content type");
 }
---
+++ packages/config/index.ts
@@ -0,0 +1,8 @@
+export type Config = {
+    dir: string,
+    name: string,
+    domain: string,
+    port: string,
+    email: string,
+    password: string,
+}
