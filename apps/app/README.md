f6c389fe821253fecc960faf2c08cd5d32d21051
henri <henri.me@skiff.com>
2024-08-30 21:48:53 +0200

    removed react native

--- apps/app/README.md
+++
@@ -1,3 +0,0 @@
-# The selfmail mobile app
-
-Avaiable in the app store and the google play store.
--- apps/app/jest-setup.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/react-native/extend-expect';
-
-// react-hook form setup for testing
-// @ts-ignore
-global.window = {};
-// @ts-ignore
-global.window = global;
