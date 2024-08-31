675ea8361b42cb7f32aad71d8f8fdec84366c299
henri <henri.me@skiff.com>
2024-08-31 11:25:10 +0200

    added the vite-electron boilerplaite code

---
+++ apps/desktop/src/renderer/src/main.tsx
@@ -0,0 +1,13 @@
+import './assets/main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Router from './router';
+
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>
+)
