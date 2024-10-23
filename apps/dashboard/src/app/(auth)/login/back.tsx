4c9ba4fd40ad5bfe8ec586bbb3f5595fb57987ec
henri <henri.me@skiff.com>
2024-10-24 00:22:28 +0200

    implement better back button

--- apps/dashboard/src/app/(auth)/login/back.tsx
+++ apps/dashboard/src/app/(auth)/login/back.tsx
@@ -6,11 +6,17 @@ import { useRouter } from "next/navigation";
 export default function BackButton() {
   const router = useRouter();
   return (
-    <ChevronLeft
-      onClick={() => {
-        router.back();
-      }}
-      className="absolute left-5 top-5 h-5 w-5 cursor-pointer"
-    />
+    <>
+      {
+        window && window.history.length > 1 && (
+          <ChevronLeft
+            onClick={() => {
+              router.back();
+            }}
+            className="absolute left-5 top-5 h-5 w-5 cursor-pointer"
+          />
+        )
+      }
+    </>
   );
 }
