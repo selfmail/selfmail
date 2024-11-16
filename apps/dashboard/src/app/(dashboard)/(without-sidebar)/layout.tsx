bd22a81c41e680c13c734a5a9620fe25c25160e0
henri <henri.me@skiff.com>
2024-11-16 12:49:23 +0100

    added a new page to create a new team

--- apps/dashboard/src/app/(dashboard)/(without-sidebar)/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/(without-sidebar)/layout.tsx
@@ -1,13 +1,4 @@
 import TeamSidebar from "@/components/team-sidebar";
-import "@/styles/globals.css";
-
-import { type Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: "Dashboard",
-    description: "Dashboard of selfmail.",
-    icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
 
 export default function Layout({
     children,
