3887d8b6bb3ab86e8de2f8a716490ea6515ee8c6
henri <henri.me@skiff.com>
2024-12-23 17:03:18 +0100

    feat(database): add multiple model for the emails

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,5 +1,8 @@
 import { ChartEmailsSentMonth } from "@/components/charts/emails-sent/month";
 import { SetHeader } from "@/components/header";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import { Avatar, AvatarFallback } from "ui/avatar";
 import { Button } from "ui/button";
 
 export default function HomePage() {
@@ -63,15 +66,111 @@ export default function HomePage() {
 				<ChartEmailsSentMonth />
 			</div>
 			<div className="flex flex-col p-5 border-b gap-2 border-r border-border col-span-1">
-				<h2 className="text-lg font-medium">Create a new email</h2>
+				<h2 className="text-lg font-medium">Members in this workspace</h2>
 				<p className="text-sm text-muted-foreground">
-					You can create a new email by clicking the button below or by clicking
-					on the link on the sidebar.
+					You can invite members by clicking the "members" button on the
+					sidebar. This is a list of the first 5 members in this workspace.
 				</p>
-				<Button>Create email</Button>
+				<div className="flex flex-col space-y-3">
+					<div className="border border-border flex items-center justify-between p-2 rounded-lg">
+						<div className="flex space-x-3">
+							<Avatar>
+								<AvatarFallback>HN</AvatarFallback>
+							</Avatar>
+							<div className="flex flex-col">
+								<p>Henri</p>
+								<p className="text-sm text-muted-foreground">
+									henri@selfmail.app
+								</p>
+							</div>
+						</div>
+						<Button variant="ghost" size="icon">
+							<EllipsisVerticalIcon className="w-4 h-4" />
+						</Button>
+					</div>
+
+					<div className="border border-border flex items-center justify-between p-2 rounded-lg">
+						<div className="flex space-x-3">
+							<Avatar>
+								<AvatarFallback>HN</AvatarFallback>
+							</Avatar>
+							<div className="flex flex-col">
+								<p>Henri</p>
+								<p className="text-sm text-muted-foreground">
+									henri@selfmail.app
+								</p>
+							</div>
+						</div>
+						<Button variant="ghost" size="icon">
+							<EllipsisVerticalIcon className="w-4 h-4" />
+						</Button>
+					</div>
+
+					<div className="border border-border flex items-center justify-between p-2 rounded-lg">
+						<div className="flex space-x-3">
+							<Avatar>
+								<AvatarFallback>HN</AvatarFallback>
+							</Avatar>
+							<div className="flex flex-col">
+								<p>Henri</p>
+								<p className="text-sm text-muted-foreground">
+									henri@selfmail.app
+								</p>
+							</div>
+						</div>
+						<Button variant="ghost" size="icon">
+							<EllipsisVerticalIcon className="w-4 h-4" />
+						</Button>
+					</div>
+
+					<div className="border border-border flex items-center justify-between p-2 rounded-lg">
+						<div className="flex space-x-3">
+							<Avatar>
+								<AvatarFallback>HN</AvatarFallback>
+							</Avatar>
+							<div className="flex flex-col">
+								<p>Henri</p>
+								<p className="text-sm text-muted-foreground">
+									henri@selfmail.app
+								</p>
+							</div>
+						</div>
+						<Button variant="ghost" size="icon">
+							<EllipsisVerticalIcon className="w-4 h-4" />
+						</Button>
+					</div>
+
+					<div className="border border-border flex items-center justify-between p-2 rounded-lg">
+						<div className="flex space-x-3">
+							<Avatar>
+								<AvatarFallback>HN</AvatarFallback>
+							</Avatar>
+							<div className="flex flex-col">
+								<p>Henri</p>
+								<p className="text-sm text-muted-foreground">
+									henri@selfmail.app
+								</p>
+							</div>
+						</div>
+						<Button variant="ghost" size="icon">
+							<EllipsisVerticalIcon className="w-4 h-4" />
+						</Button>
+					</div>
+				</div>
 			</div>
 			<div className="flex flex-col p-5 border-b border-border">
 				<h2 className="text-lg font-medium">Recent Updates</h2>
+				<Image
+					src="/images/updates.png"
+					alt="updates"
+					width={500}
+					height={350}
+				/>
+				<p>
+					In this new update, we are adding a new feature to the app. We are
+					adding a new feature to the app. We are adding a new feature to the
+					app.
+				</p>
 			</div>
 		</div>
 	);
