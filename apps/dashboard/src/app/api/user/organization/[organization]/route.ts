1a32be263b7b406659f1dbe4785b9488a7138240
henri <henri.me@skiff.com>
2024-12-28 19:26:07 +0100

    feat(dashboard, api): add better-fetch and improve user session handling
    - Introduced @better-fetch/fetch for enhanced API calls in the dashboard.
    - Updated user session management in the organization API route to utilize better-fetch, improving error handling and logging.
    - Added new dependencies for better-auth and better-fetch to package.json for improved functionality.

--- apps/dashboard/src/app/api/user/organization/[organization]/route.ts
+++ apps/dashboard/src/app/api/user/organization/[organization]/route.ts
@@ -1,8 +1,28 @@
 // Get information about if the user is in a team
 
+import { betterFetch } from "@better-fetch/fetch";
 import { auth } from "auth";
+import type { Session } from "better-auth";
 import { db } from "database";
 import { NextResponse } from "next/server";
+
+type SessionData = {
+	session: Session;
+	user: {
+		id: string;
+		name: string;
+		email: string;
+		emailVerified: boolean;
+		image: string | null;
+		createdAt: string;
+		updatedAt: string;
+		role: string;
+		banned: boolean;
+		banReason: string | null;
+		banExpires: string | null;
+		username: string;
+	};
+};
 
 // Checks if the organization is the current active organization. If not, this will be updated. If the user
 // doesn't belongs to this organization, he has to login again with an error code.
@@ -12,12 +32,18 @@ export async function GET(
 ) {
 	const organization = (await params).organization;
 
-	const user = await auth.api.getSession({
-		headers: req.headers,
-	});
-	console.log(`This is the user: ${user}`);
+	const { data: session } = await betterFetch<SessionData>(
+		"/api/auth/get-session",
+		{
+			baseURL: "http://localhost:4000",
+			headers: {
+				cookie: req.headers.get("cookie") || "",
+			},
+		},
+	);
+	console.log(`This is the user: ${session?.user}`);
 
-	if (!user) {
+	if (!session) {
 		return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 	}
 
@@ -30,7 +56,7 @@ export async function GET(
 		// orgs are not matching, checking if the user is inside this org
 		const members = await db.member.findMany({
 			where: {
-				userId: user.user.id,
+				userId: session.user.id,
 				organizationId: organization,
 			},
 		});
