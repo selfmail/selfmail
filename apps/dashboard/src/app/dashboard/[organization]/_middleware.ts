48539ad4f0b7ac98b211d06f526166670af67abf
henri <henri.me@skiff.com>
2025-01-11 11:03:00 +0100

    feat(dashboard, api): enhance user session management and organization validation
    - Added 'consola' dependency for improved logging capabilities.
    - Refactored organization API route to utilize NextRequest and cookies for session validation, enhancing security.
    - Updated dashboard middleware to streamline session and organization checks, improving user redirection logic.
    - Enhanced error handling for unauthorized access, ensuring better user experience during organization transitions.

--- apps/dashboard/src/app/dashboard/[organization]/_middleware.ts
+++ apps/dashboard/src/app/dashboard/[organization]/_middleware.ts
@@ -1,27 +1,9 @@
 // dashboard middleware
 
-import { betterFetch } from "@better-fetch/fetch";
 import type { MiddlewareFunctionProps } from "@rescale/nemo";
-import type { Session } from "better-auth";
+import { auth } from "auth";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-
-type SessionData = {
-	session: Session;
-	user: {
-		id: string;
-		name: string;
-		email: string;
-		emailVerified: boolean;
-		image: string | null;
-		createdAt: string;
-		updatedAt: string;
-		role: string;
-		banned: boolean;
-		banReason: string | null;
-		banExpires: string | null;
-		username: string;
-	};
-};
 
 export const dashboardMiddleware = async ({
 	request,
@@ -30,51 +12,19 @@ export const dashboardMiddleware = async ({
 	event,
 	params,
 }: MiddlewareFunctionProps) => {
-	let user: undefined | string = undefined;
-
-	if (!context.has("user")) {
-		const { data: session } = await betterFetch<SessionData>(
-			"/api/auth/get-session",
-			{
-				baseURL: request.nextUrl.origin,
-				headers: {
-					cookie: request.headers.get("cookie") || "",
-				},
-			},
-		);
-		if (session?.session?.userId) {
-			context.set("user", session.session.userId);
-			user = session.session.userId;
-		}
-	} else {
-		user = context.get("user") as string | undefined;
-	}
-
-	if (!user) {
-		return NextResponse.redirect(
-			new URL(
-				"/auth/login?message=You%20must%20be%20logged%20in%20to%20access%20this%20page",
-				request.url,
-			),
-		);
-	}
-
-	// checking the current org
-
-	const orgId: string = params().organization?.[0] as string;
-
-	const { data: organization, error } = await betterFetch<{
-		isMember?: boolean;
-		error?: string;
-	}>(`/api/user/organization/${orgId}`, {
-		baseURL: request.nextUrl.origin,
+	await Promise.all([
+		auth.api.getSession({
+			headers: await headers(),
+		}),
+		auth.api.listSessions({
+			headers: await headers(),
+		}),
+		auth.api.getFullOrganization({
+			headers: await headers(),
+		}),
+	]).catch((e) => {
+		const url = request.nextUrl.clone()
+		url.pathname = '/auth/login'
+		return NextResponse.redirect(url)
 	});
-
-	if (organization?.error || error?.statusText)
-		console.log(`
-status: ${error?.status}
-statusText: ${error?.statusText}
-
-response: ${organization}
-		`);
 };
