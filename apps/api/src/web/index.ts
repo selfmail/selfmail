d926674e4d4df767a12ca329e139b4ca748fe95b
i-am-henri <henri.gg@icloud.com>
2025-07-11 20:45:24 +0000

    feat(api): implemented rate-limiting for dashboard api

--- apps/api/src/web/index.ts
+++ apps/api/src/web/index.ts
@@ -1,5 +1,7 @@
 import { cors } from "@elysiajs/cors";
-import Elysia, { t } from "elysia";
+import Elysia, { status, t } from "elysia";
+import z from "zod";
+import { unkey } from "../lib/unkey";
 
 export const web = new Elysia({ name: "Web", prefix: "/web" })
 	.use(
@@ -17,6 +19,35 @@ export const web = new Elysia({ name: "Web", prefix: "/web" })
 			methods: ["GET", "POST", "PUT", "DELETE"],
 		}),
 	)
+	.onRequest(async ({ server, request, status }) => {
+		console.log(server?.requestIP(request));
+		let ip: string | undefined;
+		const ipv4 = await z
+			.ipv4()
+			.safeParseAsync(server?.requestIP(request)?.address || "");
+		const ipv6 = await z
+			.ipv6()
+			.safeParseAsync(server?.requestIP(request)?.address || "");
+
+		if (!ipv4.success && !ipv6.success) {
+			throw status(400, "Invalid IP address format");
+		}
+
+		if (ipv4.success) {
+			ip = ipv4.data;
+		} else if (ipv6.success) {
+			ip = ipv6.data;
+		}
+		if (!ip) {
+			throw status(400, "IP address not found");
+		}
+
+		const ratelimit = await unkey.limit(ip);
+
+		if (!ratelimit.success) {
+			return new Response("try again later", { status: 429 });
+		}
+	})
 	.get("/", () => "<h1>Hello World</h1>", {
 		afterHandle({ response, set }) {
 			set.headers["Content-Type"] = "text/html; charset=utf8";
