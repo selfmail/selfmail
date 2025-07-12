d91b0fee548096ce600c166acb5f9d01bd533659
i-am-henri <henri.gg@icloud.com>
2025-07-12 15:36:21 +0000

    feat(web): created new web project

---
+++ apps/dashboard/src/lib/client.ts
@@ -0,0 +1,4 @@
+import { treaty } from "@elysiajs/eden";
+import type { App } from "../../../api/src/index";
+
+export const client = treaty<App>("localhost:3000");
--- apps/web/src/lib/client.ts
+++
@@ -1,4 +0,0 @@
-import { treaty } from "@elysiajs/eden";
-import type { App } from "@/../api/src/index";
-
-export const client = treaty<App>("localhost:3000");
