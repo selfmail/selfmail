a7becd27d5516d19e45769cf4eb87dcb26fa0acf
i-am-henri <henri.gg@icloud.com>
2025-08-29 11:49:21 +0200

    chore(relay): add mx records cache

--- apps/relay/src/services/dns.ts
+++ apps/relay/src/services/dns.ts
@@ -1,13 +1,26 @@
 import { resolveMx } from "node:dns/promises";
 import { Logs } from "services/logs";
+import { Cache } from "./cache";
 
 export abstract class DNS {
 	static async resolveMX(
 		domain: string,
 	): Promise<{ exchange: string; priority: number }[]> {
-		const record = await resolveMx(domain);
+		const now = Date.now();
+		const entry = await Cache.get(domain);
 
-		Logs.log(`Found: ${JSON.stringify(record)} for ${domain}`);
-		return record;
+		if (entry && entry.expires > now) {
+			// domain in cache found, return records
+			Logs.log(`Used ${domain} from cache!`);
+			return entry.records;
+		}
+
+		// domain not found in records, creating new cache record
+		const records = await resolveMx(domain);
+		const ttl = 3600 * 1000; // default fallback
+		Cache.set(domain, { records, expires: now + ttl });
+
+		Logs.log(`Found: ${JSON.stringify(records)} for ${domain}`);
+		return records;
 	}
 }
