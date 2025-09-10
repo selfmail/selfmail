be88a3924035850afa84c3f862ac00508ac089b2
i-am-henri <henri.gg@icloud.com>
2025-09-10 20:29:59 +0200

    chore(payments): add counting features for bytes

--- apps/payments/src/queue/downgrade.ts
+++ apps/payments/src/queue/downgrade.ts
@@ -11,15 +11,46 @@ export interface DowngradeJobData {
 	downgradeAt: Date;
 }
 
+
+// TODO: add logs to keep track of errors
 export async function downgrade(job: Job<DowngradeJobData, void, string>) {
 	const workspace = await db.workspace.findUnique({
 		where: { id: job.data.workspaceId },
-		select: { id: true,  },
+		select: { id: true, },
 	});
 
 	if (!workspace) throw new Error("Workspace not found");
 
 	// check for used resources
+	const workspaceUsedBytes = await db.email.aggregate({
+		_sum: { sizeBytes: true },
+		where: {
+			address: {
+				MemberAddress: {
+					some: {
+						member: {
+							workspaceId: workspace.id
+						}
+					}
+				}
+			}
+		}
+	});
+
+	if (!workspaceUsedBytes?._sum) throw new Error("Could not calculate used resources");
+
+	const newPlan = await db.plan.findUnique({
+		where: {
+			name: job.data.plan
+		}
+	})
+
+	if (!newPlan) throw new Error("Plan not found")
+
+	if (workspaceUsedBytes._sum.sizeBytes && workspaceUsedBytes._sum.sizeBytes > newPlan.storageBytes) {
+		// TODO: keep track of too much storage usage attempts
+		throw new Error("Cannot downgrade: used storage exceeds new plan's limit");
+	}
 
 	const update = await db.workspace.update({
 		where: {
