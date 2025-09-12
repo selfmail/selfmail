a71bb14478b05ea7264ec0bee93b0f2fc013daea
i-am-henri <henri.gg@icloud.com>
2025-09-12 23:59:38 +0200

    chore(admin): add next app to manage the platform

---
+++ apps/admin/src/actions/auth/register.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import z from "zod";
+import { auth } from "@/lib/auth";
+import { requireUnauthenticated } from "../auth";
+
+const schema = z.object({
+	email: z.email(),
+	password: z.string().min(8),
+	name: z.string().min(2)
+});
+
+export const register = requireUnauthenticated
+	.inputSchema(schema)
+	.action(async ({ parsedInput }) => {
+		const user = await auth.api.signUpEmail({
+			body: {
+				email: parsedInput.email,
+				password: parsedInput.password,
+				name: parsedInput.name,
+				rememberMe: true,
+			},
+		});
+
+		if (!user.user) throw new Error("Failed to login user.");
+
+		return redirect("/");
+	});
---
+++ apps/admin/src/app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+export default function Register() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <form action={}>
+
+            </form>
+        </div>
+    )
+}
