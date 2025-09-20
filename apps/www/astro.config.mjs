9e9dfdf5335dd355187fe77977577cf87a314bee
i-am-henri <henri.gg@icloud.com>
2025-09-20 22:39:07 +0200

    chore(www): add node adapter

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -4,15 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import bun from 'astro-bun-adapter';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	i18n: {
-		locales: ["es", "en", "pt", "fr", "de", "it", "ja", "ko", "zh-CN", "zh-TW"],
-		defaultLocale: "en",
-	},
-	output: 'server',
-  adapter: bun(),
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    i18n: {
+        locales: ["es", "en", "pt", "fr", "de", "it", "ja", "ko", "zh-CN", "zh-TW"],
+        defaultLocale: "en",
+    },
+    output: 'server',
+  adapter: node({
+    mode: "standalone",
+  }),
 });
--- apps/www/src/actions/index.ts
+++ apps/www/src/actions/index.ts
@@ -1,19 +1,18 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
 import { neon } from "@neondatabase/serverless";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
-import { defineAction } from "astro:actions";
-import { z } from "astro:schema";
 
 const ratelimit = new Ratelimit({
-  redis: new Redis({
-    url: import.meta.env.UPSTASH_REDIS_REST_URL,
-    token: import.meta.env.UPSTASH_REDIS_REST_TOKEN,
-  }),
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
+	redis: new Redis({
+		url: import.meta.env.UPSTASH_REDIS_REST_URL,
+		token: import.meta.env.UPSTASH_REDIS_REST_TOKEN,
+	}),
+	limiter: Ratelimit.slidingWindow(10, "10 s"),
 });
 
 const sql = neon(import.meta.env.DATABASE_URL);
-
 
 export const server = {
 	signWaitlist: defineAction({
@@ -22,15 +21,15 @@ export const server = {
 			email: z.string().email(),
 		}),
 		handler: async (input) => {
-            const identifier = "waitlist-api:" + input.email;
-            const { success } = await ratelimit.limit(identifier);
+			const identifier = "waitlist-api:" + input.email;
+			const { success } = await ratelimit.limit(identifier);
 
-            if (!success) {
-                console.log(success)
-              return "Unable to process at this time";
-            }
+			if (!success) {
+				console.log(success);
+				return "Unable to process at this time";
+			}
 
-            await sql`INSERT INTO "Waitlist" (email) VALUES (${input.email}) ON CONFLICT (email) DO NOTHING;`;
+			await sql`INSERT INTO "Waitlist" (email) VALUES (${input.email}) ON CONFLICT (email) DO NOTHING;`;
 
 			console.log("Received waitlist sign-up:", input.email);
 		},
