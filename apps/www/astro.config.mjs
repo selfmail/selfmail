ded229df0b367d7f4b867e62041c48dea95549e5
i-am-henri <henri.gg@icloud.com>
2025-09-20 15:07:41 +0200

    chore(www): add lingo to support multiple languages

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	i18n: {
+		locales: ["es", "en", "pt", "fr", "de", "it", "ja", "ko", "zh-CN", "zh-TW"],
+		defaultLocale: "en",
+	},
+});
