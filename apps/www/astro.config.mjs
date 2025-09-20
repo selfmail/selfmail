6ce6802a7102406d533b8006515b2b7d3e732720
i-am-henri <henri.gg@icloud.com>
2025-09-20 23:12:58 +0200

    chore(www): add host

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,22 +1,23 @@
 // @ts-check
 
+import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import bun from 'astro-bun-adapter';
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    i18n: {
-        locales: ["es", "en", "pt", "fr", "de", "it", "ja", "ko", "zh-CN", "zh-TW"],
-        defaultLocale: "en",
-    },
-    output: 'server',
-  adapter: node({
-    mode: "standalone",
-  }),
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	i18n: {
+		locales: ["es", "en", "pt", "fr", "de", "it", "ja", "ko", "zh-CN", "zh-TW"],
+		defaultLocale: "en",
+	},
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
+	server: {
+		host: "0.0.0.0",
+	},
 });
