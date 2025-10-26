7b730b0ce98bb79cdac1b889c79ab999adc0c9d9
i-am-henri <henri.gg@icloud.com>
2025-10-26 13:24:52 +0100

    chore(api): add core functionality for email webhook

--- apps/api/src/email-ws/index.ts
+++ apps/api/src/email-ws/index.ts
@@ -1,4 +1,17 @@
-import Elysia from "elysia";
+import { Worker } from "bullmq";
+import Elysia, { t } from "elysia";
+import IORedis from "ioredis";
+import {
+	middlewareAuthentication,
+	verifyWorkspaceMembership,
+} from "../lib/auth";
+
+export const connection = new IORedis(
+	process.env.REDIS_URL ?? "redis://localhost:6379",
+	{
+		maxRetriesPerRequest: null,
+	},
+);
 
 export const emailWebsocket = new Elysia({
 	prefix: "/ws/email",
@@ -7,13 +20,59 @@ export const emailWebsocket = new Elysia({
 			"WebSocket endpoint for email events, so forward them into the frontend.",
 	},
 }).ws("/", {
-	open(ws) {
-		console.log("WebSocket opened:", ws.id);
+	cookie: t.Cookie({
+		"selfmail-session-token": t.String({
+			description: "Session token for authentication",
+		}),
+	}),
+	query: t.Object({
+		workspaceSlug: t.String({
+			description: "The slug of the workspace to connect to",
+		}),
+	}),
+	async open(ws) {
+		// Validating current session with token from cookie
+		const sessionToken = ws.data.cookie["selfmail-session-token"].value;
+
+		if (!sessionToken || sessionToken.length === 0) {
+			ws.close(1008, "Invalid session token");
+			return;
+		}
+
+		// Check the session token against your authentication system here
+		const { authenticated, user } =
+			await middlewareAuthentication(sessionToken);
+
+		if (!authenticated || !user) {
+			ws.close(1008, "Authentication failed");
+			return;
+		}
+
+		const { isMember, member, workspace } = await verifyWorkspaceMembership(
+			user.id,
+			ws.data.query.workspaceSlug,
+		);
+
+		if (!isMember || !member || !workspace) {
+			ws.close(1008, "Workspace membership verification failed");
+			return;
+		}
+
+		ws.send(`Connected to workspace: ${workspace.name}`);
 	},
 	close(ws) {
 		console.log("WebSocket closed:", ws.id);
 	},
 	message(ws, message) {
-		console.log("WebSocket message:", ws.id, message);
+		const worker = new Worker<{}, void>(
+			"foo",
+			async (job) => {
+				ws.send(job.data);
+			},
+			{
+				connection,
+				concurrency: 5,
+			},
+		);
 	},
 });
---
+++ apps/api/src/lib/auth.ts
@@ -0,0 +1,90 @@
+import { db } from "database";
+
+export const middlewareAuthentication = async (
+	sessionToken: string | undefined,
+) => {
+	try {
+		if (!sessionToken) {
+			return {
+				authenticated: false,
+			};
+		}
+
+		const session = await db.session.findFirst({
+			where: {
+				token: sessionToken,
+			},
+			include: {
+				user: true,
+			},
+		});
+
+		if (!session) {
+			return {
+				authenticated: false,
+			};
+		}
+
+		if (session.expiresAt < new Date()) {
+			await db.session.deleteMany({
+				where: {
+					token: sessionToken,
+				},
+			});
+
+			return {
+				authenticated: false,
+			};
+		}
+
+		return {
+			authenticated: true,
+			user: session.user,
+		};
+	} catch (_) {
+		return {
+			authenticated: false,
+		};
+	}
+};
+
+export const verifyWorkspaceMembership = async (
+	userId: string,
+	workspaceSlug: string,
+) => {
+	const workspace = await db.workspace.findUnique({
+		where: {
+			slug: workspaceSlug,
+		},
+	});
+
+	if (!workspace) {
+		return {
+			isMember: false,
+		};
+	}
+
+	const member = await db.member.findUnique({
+		where: {
+			userWorkspaceId: {
+				userId,
+				workspaceId: workspace.id,
+			},
+		},
+		include: {
+			MemberPermission: true,
+		},
+	});
+
+	if (!member) {
+		return {
+			isMember: false,
+		};
+	}
+
+	return {
+		isMember: true,
+		member,
+		workspace,
+	};
+};
