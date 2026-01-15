4298d3286fa6f0c743aa0bbf3cde49869601cb03
i-am-henri <henri.gg@icloud.com>
2026-01-15 19:08:06 +0100

    chore(api): removed billing infrastrucuture for beta

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,7 @@
 import { Elysia } from "elysia";
-import { billing } from "./billing";
 
-const app = new Elysia()
-	.get("/", () => "Hello Elysia")
-	.use(billing)
-	.listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
--- packages/transactional/index.tsx
+++ packages/transactional/index.tsx
@@ -1,95 +1,18 @@
 import { pretty, render, toPlainText } from "@react-email/render";
-import DowngradeTemplate from "./emails/billing-downgrade";
-import UpgradeTemplate from "./emails/billing-upgrade";
-import OverlimitTemplate from "./emails/downgrade-overlimit";
 import VerifyEmailTemplate from "./emails/verify-email";
 
 export async function generateVerifyEmailTemplate({
-	name,
-	token,
+  name,
+  token,
 }: {
-	name: string;
-	token: number;
+  name: string;
+  token: number;
 }) {
-	const html = await pretty(
-		await render(<VerifyEmailTemplate name={name} token={token} />),
-	);
-	return {
-		html,
-		text: toPlainText(html),
-	};
-}
-
-export async function generateBillingDowngradeTemplate({
-	oldPlan,
-	newPlan,
-	workspaceName,
-	name,
-}: {
-	oldPlan: string;
-	newPlan: string;
-	name: string;
-	workspaceName: string;
-}) {
-	const html = await pretty(
-		await render(
-			<DowngradeTemplate
-				oldPlan={oldPlan}
-				newPlan={newPlan}
-				workspaceName={workspaceName}
-				name={name}
-			/>,
-		),
-	);
-	return {
-		html,
-		text: toPlainText(html),
-	};
-}
-export async function generateOverlimitTemplate({
-	oldPlan,
-	newPlan,
-	workspaceName,
-	name,
-}: {
-	oldPlan: string;
-	newPlan: string;
-	name: string;
-	workspaceName: string;
-}) {
-	const html = await pretty(
-		await render(
-			<OverlimitTemplate
-				oldPlan={oldPlan}
-				newPlan={newPlan}
-				workspaceName={workspaceName}
-				name={name}
-			/>,
-		),
-	);
-	return {
-		html,
-		text: toPlainText(html),
-	};
-}
-
-export async function generateBillingUpgradeTemplate({
-	workspaceName,
-	name,
-}: {
-	name: string;
-	workspaceName: string;
-}) {
-	const html = await pretty(
-		await render(
-			<UpgradeTemplate
-				workspaceName={workspaceName}
-				name={name}
-			/>,
-		),
-	);
-	return {
-		html,
-		text: toPlainText(html),
-	};
+  const html = await pretty(
+    await render(<VerifyEmailTemplate name={name} token={token} />)
+  );
+  return {
+    html,
+    text: toPlainText(html),
+  };
 }
