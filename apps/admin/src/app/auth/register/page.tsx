3a9766aeaf00f35e70e53da20b46a31a2a758987
i-am-henri <henri.gg@icloud.com>
2026-01-15 18:44:54 +0100

    chore: removed unused projects

--- apps/admin/src/app/auth/register/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Register() {
-    return (
-        <div className="min-h-screen flex items-center justify-center">
-            <form action={}>
-
-            </form>
-        </div>
-    )
-}
