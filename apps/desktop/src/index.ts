4a1606a3ac72aad81028746bb3b5dd771aab2606
i-am-henri <henri.gg@icloud.com>
2026-03-07 12:43:04 +0100

    chore: started electron app

---
+++ apps/desktop/src/index.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from "electron";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    title: "skjf",
+  });
+
+  win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
