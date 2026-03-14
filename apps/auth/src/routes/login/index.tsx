e222145f31a9f0cb78df471e0bbf16200f577047
i-am-henri <henri.gg@icloud.com>
2026-03-14 15:13:56 +0100

    chore(auth): improved button design

--- apps/auth/src/routes/login/index.tsx
+++ apps/auth/src/routes/login/index.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Building2Icon, KeyRoundIcon } from "lucide-react";
+import { Google } from "#/components/ui/svgs/google";
 
 export const Route = createFileRoute("/login/")({
   component: RouteComponent,
@@ -18,11 +20,11 @@ function RouteComponent() {
         <form className="flex flex-col gap-4 pt-2">
           <input
             className="w-full rounded-full border-2 border-neutral-200 px-6 py-3 outline-none ring-neutral-200 transition-colors duration-200 focus-within:border-neutral-400 focus-within:ring-2 focus:outline-none"
-            placeholder="Ihre Email Adresse"
+            placeholder="Your E-Mail Address"
             type="email"
           />
           <button
-            className="2 w-full cursor-pointer rounded-full bg-neutral-900 px-6 py-3 text-white transition-colors duration-200 focus-within:bg-neutral-700 focus-within:ring-2 focus-within:ring-neutral-700 focus-within:ring-offset-2 hover:bg-neutral-700 focus:outline-none disabled:cursor-not-allowed disabled:bg-neutral-300 disabled:text-neutral-500"
+            className="2 hit-area-2 w-full cursor-pointer rounded-full bg-neutral-900 px-6 py-3 text-white transition-colors duration-200 focus-within:bg-neutral-700 focus-within:ring-2 focus-within:ring-neutral-700 focus-within:ring-offset-2 hover:bg-neutral-700 focus:outline-none disabled:cursor-not-allowed disabled:bg-neutral-300 disabled:text-neutral-500"
             type="submit"
           >
             Proceed
@@ -30,27 +32,37 @@ function RouteComponent() {
           <div className="h-0.5 w-full rounded-full bg-neutral-200" />
           <div className="flex flex-col gap-2">
             <button
-              className="cursor-pointer rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
+              className="relative flex w-full cursor-pointer items-center justify-start rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
               type="button"
             >
-              Sign in with Google
+              <Google className="absolute left-6 h-4 w-4" />
+              <span className="ml-8 w-full text-left">Sign in with Google</span>
             </button>
             <button
-              className="cursor-pointer rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
+              className="relative flex w-full cursor-pointer items-center justify-start rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
               type="button"
             >
-              Sign in with Passkey
+              <KeyRoundIcon className="absolute left-6 h-4 w-4" />
+
+              <span className="ml-8 w-full text-left">
+                Sign in with Passkey
+              </span>
             </button>
             <button
-              className="cursor-pointer rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
+              className="relative flex w-full cursor-pointer items-center justify-start rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
               type="button"
             >
-              Enterprises
+              <Building2Icon className="absolute left-6 h-4 w-4" />
+
+              <span className="ml-8 w-full text-left">Enterprises</span>
             </button>
           </div>
           <p className="pt-4 text-center">
             Don't have an Account?{" "}
-            <a className="text-blue-500 hover:underline" href="/register">
+            <a
+              className="hit-area-2 text-blue-500 hover:underline"
+              href="/register"
+            >
               Register here
             </a>
           </p>
--- apps/auth/src/routes/register/index.tsx
+++ apps/auth/src/routes/register/index.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Building2Icon } from "lucide-react";
+import { Google } from "#/components/ui/svgs/google";
 
 export const Route = createFileRoute("/register/")({
   component: RouteComponent,
@@ -28,7 +30,7 @@ function RouteComponent() {
           />
           <input
             className="w-full rounded-full border-2 border-neutral-200 px-6 py-3 outline-none ring-neutral-200 transition-colors duration-200 focus-within:border-neutral-400 focus-within:ring-2 focus:outline-none"
-            placeholder="Your Email Address"
+            placeholder="Your E-Mail Address"
             type="email"
           />
           <input
@@ -37,7 +39,7 @@ function RouteComponent() {
             type="password"
           />
           <button
-            className="w-full cursor-pointer rounded-full bg-neutral-900 px-6 py-3 text-white transition-colors duration-200 focus-within:bg-neutral-700 focus-within:ring-2 focus-within:ring-neutral-700 focus-within:ring-offset-2 hover:bg-neutral-700 focus:outline-none disabled:cursor-not-allowed disabled:bg-neutral-300 disabled:text-neutral-500"
+            className="hit-area-4 w-full cursor-pointer rounded-full bg-neutral-900 px-6 py-3 text-white transition-colors duration-200 focus-within:bg-neutral-700 focus-within:ring-2 focus-within:ring-neutral-700 focus-within:ring-offset-2 hover:bg-neutral-700 focus:outline-none disabled:cursor-not-allowed disabled:bg-neutral-300 disabled:text-neutral-500"
             type="submit"
           >
             Create Account
@@ -45,21 +47,27 @@ function RouteComponent() {
           <div className="h-0.5 w-full rounded-full bg-neutral-200" />
           <div className="flex flex-col gap-2">
             <button
-              className="cursor-pointer rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
+              className="relative flex w-full cursor-pointer items-center justify-start rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
               type="button"
             >
-              Sign up with Google
+              <Google className="absolute left-6 h-4 w-4" />
+              <span className="ml-8 w-full text-left">Sign in with Google</span>
             </button>
             <button
-              className="cursor-pointer rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
+              className="relative flex w-full cursor-pointer items-center justify-start rounded-full border-2 border-neutral-200 px-6 py-3 transition-colors duration-200 hover:bg-neutral-100"
               type="button"
             >
-              Enterprise Setup
+              <Building2Icon className="absolute left-6 h-4 w-4" />
+
+              <span className="ml-8 w-full text-left">Enterprise Setup</span>
             </button>
           </div>
           <p className="pt-4 text-center">
             Already have an Account?{" "}
-            <a className="text-blue-500 hover:underline" href="/login">
+            <a
+              className="hit-area-2 text-blue-500 hover:underline"
+              href="/login"
+            >
               Sign in here
             </a>
           </p>
